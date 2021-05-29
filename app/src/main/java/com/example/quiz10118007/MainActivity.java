6c57b260134ae6c29bb54fb1508ca353479c9663
RamaA-H <55701022+RamaA-H@users.noreply.github.com>
2021-05-29 12:17:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118007/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.quiz10118007;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Tanggal Pengerjaan : 29 Mei 2021
+// NIM                : 10118007
+// Nama               : Rama Al Halik
+// Kelas              : IF-1
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
